ad7d5f236b043959a060226325922e40792d6c46
Sheja Eddy <shejaeddy05@gmail.com>
2023-09-08 14:59:07 +0200

    Made some UI fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,24 +7,28 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <p>Welcome back {{ explode(" ", auth()->user()->name)[0] }}.</p>
 {{--   Cards for  "totalTransactions", "totalTransactionsAmount", "todayTransactionsAmount", "studentsNumber"--}}
     <div class="row">
         <div class="col-md-3">
-            <div class="card card-body bg-red">
-                {{ $totalTransactions }} Total Transactions
+            <div class="card card-body bg-red text-center">
+                {{ $totalTransactions }}
+                <p>Total Transactions</p>
             </div>
         </div><div class="col-md-3 ">
-            <div class="card card-body bg-blue">
-                {{ $totalTransactionsAmount }} Total Amount Earned
+            <div class="card card-body bg-blue text-center">
+                {{ $totalTransactionsAmount }}
+                <p>Rwf Total Amount Earned</p>
             </div>
         </div><div class="col-md-3">
-            <div class="card card-body bg-indigo">
-                {{ $todayTransactionsAmount }} Today's Transactions amount
+            <div class="card card-body bg-indigo text-center">
+                {{ $todayTransactionsAmount }} Rwf
+                <p>Today's Transactions amount</p>
             </div>
         </div><div class="col-md-3">
-            <div class="card card-body bg-green">
-                {{ $studentsNumber}} Students number
+            <div class="card card-body bg-green text-center">
+                {{ $studentsNumber}}
+                <p>Students number</p>
             </div>
         </div>
     </div>
